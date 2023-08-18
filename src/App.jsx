25f384ac8d759a8f49d425464d0c68e44af3faaa
webdev-ashishk <webdev.ashishk@gmail.com>
2023-08-18 09:15:06 +0530

    added background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import Layout from "./components/Layout";
+import Header from "./components/Header";
 function App() {
   return (
     <>
+      <Header />
       <Layout />
     </>
   );
